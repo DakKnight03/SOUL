29cfc82711131c4c70504a19381e0f256a69c731
Dư Võ <tinduvo@Dus-MacBook-Pro.local>
2019-08-18 16:36:09 +0700

    1st

---
+++ index.js
@@ -0,0 +1,18 @@
+$("button").click(function(e) {
+    console.log($(e.target).attr("video"))
+
+})
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://love-calculator.p.rapidapi.com/getPercentage?fname=thanh&sname=du",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+		"x-rapidapi-key": "cedbb6e127msh6c3f85c77dc22c0p153d51jsn608d2ca695b6"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
